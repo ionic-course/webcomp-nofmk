0fc24121878f893493f8b9d7743b6f1278a80d31
Diego Chavez <diegochavezcarro@gmail.com>
2019-07-12 23:44:42 -0300

    fin comp sin angular

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
 const expensesList = document.querySelector('#expenses-list');
 const totalExpensesOutput = document.querySelector('#total-expenses');
+const alertCtrl = document.querySelector('ion-alert-controller');
 
 let totalExpenses = 0;
 
@@ -17,7 +18,13 @@ confirmBtn.addEventListener('click', () => {
     const enteredReason = reasonInput.value;
     const enteredAmount = amountInput.value;
     if (enteredReason.trim().lenght <= 0 || enteredAmount <= 0 || enteredAmount.trim().lenght <= 0) {
-        alert('invalid values');
+        alertCtrl.create({
+            message: 'Please enter valid reason and amount!',
+            header: 'Invalid inputs',
+            buttons : ['OKay']
+        }).then(alertElement =>{
+            alertElement.present();
+        });
         return;
     }
     const newItem = document.createElement('ion-item');
